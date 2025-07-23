389b2a1e6cab041db6b3fc1f8ff913d0638b7e6b
Ruban1864 <rubanas2005@gmail.com>
2025-07-23 21:02:46 +0530

    Axios is added

---
+++ src/axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Attach token to every request if available
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    if (userInfo && userInfo.token) {
+      config.headers.Authorization = `Bearer ${userInfo.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
